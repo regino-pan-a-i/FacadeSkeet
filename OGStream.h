7d5ca77be986f5357940abbb9b910b961513a160
Bennywass <bennywass4@gmail.com>
2024-07-19 16:50:01 -0600

    drawRectangle and setPosition

--- OGStream.h
+++ OGStream.h
@@ -6,7 +6,13 @@ class OGStream
 public:
 	void flush();
 	void setPosition(Position pos);
-	void drawRectangle(Position pos);
+	void drawRectangle(const Position& pt,
+		               double angle = 0.0,
+		               double width = 10.0,
+		               double height = 100.0,
+		               double red = 1.0,
+		               double green = 1.0,
+		               double blue = 1.0));
 	void drawLine(const Position& begin, const Position& end,
 		double red, double green, double blue);
 	void drawDot();
